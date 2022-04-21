c0d3ec37d8ab043c83b36cf478f25699603acedb
an7188 <an92818@gmail.com>
2022-04-21 23:40:26 +0900

    gateway 생성

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 // Controller와 RestContrller의 차이점은 주어진 response body를 직접 구현하느냐 주어진거 쓰느냐
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
-    @GetMapping("welcome")
+    @GetMapping("/welcome")
     public String welcome(){
         return "welcome to the First service";
     }
--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 // Controller와 RestContrller의 차이점은 주어진 response body를 직접 구현하느냐 주어진거 쓰느냐
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondServiceController {
-    @GetMapping("welcome")
+    @GetMapping("/welcome")
     public String welcome(){
         return "welcome to the Second service";
     }
