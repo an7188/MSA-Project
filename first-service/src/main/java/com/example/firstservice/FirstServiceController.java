f248eb2e06821b38c22c217cec12b2963f1a28da
an7188 <an92818@gmail.com>
2022-04-22 17:32:33 +0900

    controller에 check api 추가

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,9 +1,16 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Enumeration;
+
+@Slf4j
 @RestController
 // Controller와 RestContrller의 차이점은 주어진 response body를 직접 구현하느냐 주어진거 쓰느냐
 @RequestMapping("/first-service")
@@ -11,5 +18,17 @@ public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome to the First service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+
+        log.info(header);
+        return "Hello Word int First Service";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. This is a message from First Service ";
     }
 }
--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,9 +1,12 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 // Controller와 RestContrller의 차이점은 주어진 response body를 직접 구현하느냐 주어진거 쓰느냐
 @RequestMapping("/second-service")
@@ -11,5 +14,16 @@ public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome to the Second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header){
+
+        log.info(header);
+        return "Hello Word int Second Service";
+    }
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. This is a message from Second Service ";
     }
 }
