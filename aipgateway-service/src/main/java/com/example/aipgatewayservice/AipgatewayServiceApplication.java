1307c0c7642720af095f590e17b021f62e73edb0
an7188 <an92818@gmail.com>
2022-04-25 23:40:37 +0900

    0425 - user Microservice 강의

---
+++ aipgateway-service/src/main/java/com/example/aipgatewayservice/AipgatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.aipgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipgatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AipgatewayServiceApplication.class, args);
+    }
+
+}
