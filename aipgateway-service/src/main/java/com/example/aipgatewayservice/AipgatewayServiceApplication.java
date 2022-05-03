d90366d1f343a1abc73656f9418213825342a7c1
an7188 <an92818@gmail.com>
2022-05-03 23:54:59 +0900

    0503 실습

--- aipgateway-service/src/main/java/com/example/aipgatewayservice/AipgatewayServiceApplication.java
+++ aipgateway-service/src/main/java/com/example/aipgatewayservice/AipgatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.aipgatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AipgatewayServiceApplication {
@@ -9,5 +12,9 @@ public class AipgatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AipgatewayServiceApplication.class, args);
     }
-
+    @Bean
+    public HttpTraceRepository httpTraceRepository(){
+        // 클라이언트에서 요청했던 트레이스 정보가 메모에 담겨서 우리가 필요할때 엔드포인트로 담겨 제공
+     return new InMemoryHttpTraceRepository();
+    }
 }
